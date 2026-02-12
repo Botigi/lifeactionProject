00601afda9250cd4a6cc652e56d2fc4a3f6ec70f
thibault <botigirard@gmail.com>
2026-02-12 13:26:15 +0000

    migration du js de contact.html dans un fichier dedier contact.js

--- script/contact.js
+++ script/contact.js
@@ -1,9 +1,7 @@
-<script>
-        document.getElementById('contactForm').addEventListener('submit', function (e) {
-            e.preventDefault();
-            // Simulate sending and show confirmation
-            document.getElementById('result').style.display = 'block';
-            // Optionally clear form
-            this.reset();
-        });
-    </script>
+document.getElementById('contactForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    // Simulate sending and show confirmation
+    document.getElementById('result').style.display = 'block';
+    // Optionally clear form
+    this.reset();
+});
